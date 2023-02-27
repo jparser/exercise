346c78d5c9014c55c350e860bc7f8e974f54771f
jparser <wuonlysea@gmail.com>
2023-02-27 16:19:12 +0800

    refactor folder structure

---
+++ 0.demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>默认 Demo 页</h1>
+      <p>请修改对应的代码致具体问题并进行开发调试</p>
+    </div>
+  );
+}
+
+export default App;
