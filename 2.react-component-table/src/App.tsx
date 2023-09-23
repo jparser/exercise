d88e8d4336f75cfde7e0638250d2017280cf9e3c
MAHang098 <mahang098@163.com>
2023-09-23 15:24:08 +0800

    2.react-component-table-答案

--- 2.react-component-table/src/App.tsx
+++ 2.react-component-table/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Table } from 'antd';
 
 /**
  * 实现一个 Table 组件满足以下功能，如图 table.jpeg 所示
@@ -42,11 +43,76 @@ const testData: {
     english: 89,
   },
 ];
+const dataSource = [
+  {
+    key: '1',
+    name: 'Jim',
+    chinese: 98,
+    math: 60,
+    english: 70,
+  },
+  {
+    key: '2',
+    name: 'Tom',
+    chinese: 98,
+    math: 66,
+    english: 89,
+  },
+  {
+    key: '3',
+    name: 'Han',
+    chinese: 98,
+    math: 90,
+    english: 70,
+  },
+  {
+    key: '4',
+    name: 'Lilei',
+    chinese: 88,
+    math: 99,
+    english: 89,
+  },
+];
+
+const columns = [
+  {
+    title: 'name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'chinese',
+    dataIndex: 'chinese',
+    key: 'chinese',
+    sorter: {
+      compare: (a: { chinese: number; }, b: { chinese: number; }) => a.chinese - b.chinese,
+      multiple: 3,
+    },
+  },
+  {
+    title: 'math',
+    dataIndex: 'math',
+    key: 'math',
+    sorter: {
+      compare: (a: { math: number; }, b: { math: number; }) => a.math - b.math,
+      multiple: 2,
+    },
+  },
+  {
+    title: 'english',
+    dataIndex: 'english',
+    key: 'english',
+    sorter: {
+      compare: (a: { english: number; }, b: { english: number; }) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+];
 function App() {
   return (
     <div className="App">
       <h1>Table 组件</h1>
-      <div>使用 testData 数据在这里渲染实现的 Table 组件</div>
+      <div><Table dataSource={dataSource} columns={columns} /></div>
     </div>
   );
 }
