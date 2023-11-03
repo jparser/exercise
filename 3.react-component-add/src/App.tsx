88e1da9388892c115a70151048cbdc5ad79f854d
xichao <207875515@qq.com>
2023-11-03 18:50:45 +0800

    feat(autoIncrement): 实现加法计数器功能

--- 3.react-component-add/src/App.tsx
+++ 3.react-component-add/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './index.css';
+import React, {useState} from 'react';
+import './App.css';
 
 async function increaseRemote(a: number) {
   await new Promise((resolve) => setTimeout(resolve, Math.random() * 1e3));
@@ -12,15 +12,38 @@ async function increaseRemote(a: number) {
  * 2. 由于是异步相加，自增过程中需要将 button 置为 disabled 状态，不可响应点击；
  */
 function App() {
+ const [count, setCount] = useState(0);
+ const [isAdding, setIsAdding] = useState(false);
+
+ const handleAdd = async (amount: any)=> {
+  setIsAdding(true);
+  try {
+    const result = await increaseRemote(count + amount);
+    setCount(result);
+  } finally {
+    setIsAdding(false);
+  }
+ }
+
   return (
     <div className="App">
       <header className="App-header">
         <div>加法计数器</div>
       </header>
       <section className="App-content">
-        <button>+1</button>
-        <button>+2</button>
-        <p>数值：{'计算结果'}</p>
+        <button
+          onClick={() => handleAdd(1)}
+          disabled={isAdding}
+        >
+          +1
+        </button>
+        <button
+          onClick={() => handleAdd(2)}
+          disabled={isAdding}
+        >
+          +2
+        </button>
+        <p>数值：{count}</p>
       </section>
     </div>
   );
